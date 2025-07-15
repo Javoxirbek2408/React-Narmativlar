772fd4b055b06d116c199e5f1b3626e18d9c0b1d
Erkinov <122886346+Javoxirbek2408@users.noreply.github.com>
2025-07-15 14:05:16 +0500

    3.25

--- src/pages/Cart/CartPage.jsx
+++ src/pages/Cart/CartPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { CurrentButton } from "../../components/CurrentButton/CurrentButton";
 import { TrashIcon } from "lucide-react";
+import { Form } from "antd";
 
 export const CartPage = () => {
   const [cartpage, setCartpr] = useState([]);
@@ -18,7 +19,7 @@ export const CartPage = () => {
   };
 
   return (
-    <div className="container">
+    <div className="container !mb-[140px]">
       <div className="flex gap-2 !font-normal !text-sm l!eading-[21px]">
         <NavLink to={"/"}>Home</NavLink>/
         <NavLink to={"/cartpage"}>Cart</NavLink>
@@ -64,6 +65,46 @@ export const CartPage = () => {
           className=" mt-[60px] mb-[40px] ml-[40%]   !w-[195px] !h-[56px]  font-medium text-base leading-6 "
           title="Update Cart"
         />
+      </div>
+      <div className="flex justify-between">
+        <Form className=" flex ju items-center gap-4">
+          <Form.Item htmlFor="submit">
+            <input
+              className="w-[300px] h-[56px] p-6 !border-gray-500 border-2 rounded-md"
+              placeholder="Coupon Code"
+              type="email"
+            />
+          </Form.Item>
+          <CurrentButton
+            type="submit"
+            className={"w-[210px] !h-[56px] mb-6 !text-white !bg-[#DB4444]"}
+            title={"Apply Coupon"}
+          />
+        </Form>
+        <div className="w-[470px] !h-[324px] mt-[200px]  border-2 pt-8 ">
+          <h2 className="font-medium text-xl leading-7  pl-6">Cart Total</h2>
+          <div className="flex flex-col justify-self-start">
+            <div className="!flex  gap-[300px] pt-6 p-2  border-b-2 w-[422px] !ml-6">
+              <p>Subtotal:</p>
+              <p>$1750</p>
+            </div>
+            <div className="!flex  gap-[300px] pt-6 p-2  border-b-2 w-[422px] !ml-6">
+              <p>Shipping:</p>
+              <p>Free</p>
+            </div>
+            <div className="!flex  gap-[330px] pt-6 p-2  border-b-2 w-[422px] !ml-6">
+              <p>Total:</p>
+              <p>$1750</p>
+            </div>
+          </div>
+          <CurrentButton
+            type="submit"
+            className={
+              "w-[260px] mt-4 ml-28 !h-[56px] mb-6 !text-white !bg-[#DB4444]"
+            }
+            title={"Procees to checkout"}
+          />
+        </div>
       </div>
     </div>
   );
